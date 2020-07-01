76103b90caf57e8571f5caccf12d31d1eb257d79
davidwilson-85 <dws1985@hotmail.com>
2020-07-02 00:37:54 +0200

    -

--- toymodel_2D.py
+++ toymodel_2D.py
@@ -20,7 +20,7 @@ auxin_synthesis = 1     		# Absolute amount of molecules synthesized per cycle
 auxin_lossRate = 0.75
 
 pin1_range = (0, 9)
-pin1_UTGresponsiveness = 0.1	# Relative amount of molecules that can change cell face per cycle
+pin1_UTGresponsiveness = 0.01	# Relative amount of molecules that can change cell face per cycle
 
 cuc_range = (0, 1)
 
@@ -78,14 +78,19 @@ def create_cell_plot(matrix_shape):
 		
 		for j in range(matrix_shape[1]):
 			
-			# Draw cell outline
+			# Draw cell outline and fill
 			ImageDraw.Draw(im).polygon([(x,y),(x+cellSide,y),(x+cellSide,y+cellSide),(x,y+cellSide)], outline=(50,50,50,255), fill=index_to_rgb(lut_auxin, auxin[i,j], auxin_range))
 			
 			# Draw PIN1 (top, right, bottom, left)
-			ImageDraw.Draw(im).line([(x+1,y+2),(x+cellSide-1,y+2)], fill=index_to_rgb(lut_pin1, pin1[0,i,j], pin1_range), width=3)
-			ImageDraw.Draw(im).line([(x+cellSide-2,y+2),(x+cellSide-2,y+cellSide-1)], fill=index_to_rgb(lut_pin1, pin1[1,i,j], pin1_range), width=3)
-			ImageDraw.Draw(im).line([(x+1,y+cellSide-2),(x+cellSide-1,y+cellSide-2)], fill=index_to_rgb(lut_pin1, pin1[2,i,j], pin1_range), width=3)
-			ImageDraw.Draw(im).line([(x+2,y+2),(x+2,y+cellSide-2)], fill=index_to_rgb(lut_pin1, pin1[3,i,j], pin1_range), width=3)
+			#ImageDraw.Draw(im).line([(x+1,y+2),(x+cellSide-1,y+2)], fill=index_to_rgb(lut_pin1, pin1[0,i,j], pin1_range), width=3)
+			#ImageDraw.Draw(im).line([(x+cellSide-2,y+2),(x+cellSide-2,y+cellSide-1)], fill=index_to_rgb(lut_pin1, pin1[1,i,j], pin1_range), width=3)
+			#ImageDraw.Draw(im).line([(x+1,y+cellSide-2),(x+cellSide-1,y+cellSide-2)], fill=index_to_rgb(lut_pin1, pin1[2,i,j], pin1_range), width=3)
+			#ImageDraw.Draw(im).line([(x+2,y+2),(x+2,y+cellSide-2)], fill=index_to_rgb(lut_pin1, pin1[3,i,j], pin1_range), width=3)
+
+			ImageDraw.Draw(im).line([(x+4,y+3),(x+cellSide-4,y+3)], fill=index_to_rgb(lut_pin1, pin1[0,i,j], pin1_range), width=3)
+			ImageDraw.Draw(im).line([(x+cellSide-3,y+4),(x+cellSide-3,y+cellSide-4)], fill=index_to_rgb(lut_pin1, pin1[1,i,j], pin1_range), width=3)
+			ImageDraw.Draw(im).line([(x+4,y+cellSide-3),(x+cellSide-4,y+cellSide-3)], fill=index_to_rgb(lut_pin1, pin1[2,i,j], pin1_range), width=3)
+			ImageDraw.Draw(im).line([(x+3,y+4),(x+3,y+cellSide-4)], fill=index_to_rgb(lut_pin1, pin1[3,i,j], pin1_range), width=3)
 			
 			# Draw CUC		
 			#ImageDraw.Draw(im).ellipse([(x+15,y+15),(x+cellSide-15,y+cellSide-15)], fill='green')
@@ -99,7 +104,7 @@ def create_cell_plot(matrix_shape):
 
 	#ImageDraw.Draw(im).line([(x+1,y+2),(x+cellSide-1,y+2)], fill='red', width=3)
 
-	im.save('images/test2/image' + str(iteration) +'.png')
+	im.save('images/test/image' + str(iteration) +'.png')
 
 
 
@@ -135,61 +140,83 @@ for iteration in range(nbr_iterations):
 			if i-1 >= 0:
 				diffusionFromLeft = diffusionVectors[i-1,j]
 				nbr_cell_neighbours	+=1
-				utg_auxinRatioL = math.sqrt(auxin[i-1,j] / auxin[i,j])
 			else:
 				diffusionFromLeft = 0
-				utg_auxinRatioL = 1.0
 			
 			# Right
 			try: 
 				diffusionFromRight = diffusionVectors[i+1,j]
 				nbr_cell_neighbours	+=1
-				utg_auxinRatioR = math.sqrt(auxin[i+1,j] / auxin[i,j])
 			except IndexError:
 				diffusionFromRight = 0
-				utg_auxinRatioR = 1.0
 			
 			# Top	
 			if j-1 >= 0:
 				diffusionFromTop = diffusionVectors[i,j-1]
 				nbr_cell_neighbours	+=1
-				utg_auxinRatioT = math.sqrt(auxin[i,j-1] / auxin[i,j])
 			else:
 				diffusionFromTop = 0
-				utg_auxinRatioT = 1.0
 			
 			# Bottom
 			try: 
 				diffusionFromBottom = diffusionVectors[i,j+1]
 				nbr_cell_neighbours	+=1
-				utg_auxinRatioB = math.sqrt(auxin[i,j+1] / auxin[i,j])
 			except IndexError:
 				diffusionFromBottom = 0
-				utg_auxinRatioB = 1.0
 			
 			# Update the concentration in each cell
 			auxin[i,j] = auxin[i,j] - ( diffusionVectors[i,j] * nbr_cell_neighbours ) + diffusionFromLeft + diffusionFromRight + diffusionFromTop + diffusionFromBottom
 
+
+	# Calculate updated PIN1 allocation
+	for i in range(matrix_shape[0]):
+		for j in range(matrix_shape[1]):
+
+			# Left
+			if i-1 >= 0:
+				utg_auxinRatioL = math.sqrt(auxin[i-1,j] / auxin[i,j])
+			else:
+				utg_auxinRatioL = 1.0
+			
+			# Right
+			try:
+				utg_auxinRatioR = math.sqrt(auxin[i+1,j] / auxin[i,j])
+			except IndexError:
+				utg_auxinRatioR = 1.0
+			
+			# Top	
+			if j-1 >= 0:
+				utg_auxinRatioT = math.sqrt(auxin[i,j-1] / auxin[i,j])
+			else:
+				utg_auxinRatioT = 1.0
+			
+			# Bottom
+			try:
+				utg_auxinRatioB = math.sqrt(auxin[i,j+1] / auxin[i,j])
+			except IndexError:
+				utg_auxinRatioB = 1.0
+
 			# Calculate updated PIN1 concentration values per cell face
 			total_pin1 = pin1[0,i,j] + pin1[1,i,j] + pin1[2,i,j] + pin1[3,i,j]
 
-			updated_pin1_T = pin1[0,i,j] * utg_auxinRatioT
-			updated_pin1_R = pin1[1,i,j] * utg_auxinRatioR
-			updated_pin1_B = pin1[2,i,j] * utg_auxinRatioB
-			updated_pin1_L = pin1[3,i,j] * utg_auxinRatioL
+			updated_pin1_T = pin1[0,j,i] * utg_auxinRatioT
+			updated_pin1_R = pin1[1,j,i] * utg_auxinRatioR
+			updated_pin1_B = pin1[2,j,i] * utg_auxinRatioB
+			updated_pin1_L = pin1[3,j,i] * utg_auxinRatioL
 
 			total_pin1_updated = updated_pin1_T + updated_pin1_R + updated_pin1_B + updated_pin1_L
 
 			normalization_ratio = total_pin1 / total_pin1_updated
 
-			pin1[0,i,j] = updated_pin1_T * normalization_ratio
-			pin1[1,i,j] = updated_pin1_R * normalization_ratio
-			pin1[2,i,j] = updated_pin1_B * normalization_ratio
-			pin1[3,i,j] = updated_pin1_L * normalization_ratio
+			pin1[0,j,i] = updated_pin1_T * normalization_ratio
+			pin1[1,j,i] = updated_pin1_R * normalization_ratio
+			pin1[2,j,i] = updated_pin1_B * normalization_ratio
+			pin1[3,j,i] = updated_pin1_L * normalization_ratio
 
 
 	
-	auxin[0,0] = auxin[0,0] + auxin_synthesis
+	auxin[3,3] = auxin[3,3] + auxin_synthesis
+	auxin[9,11] = auxin[9,11] + auxin_synthesis
 
 
 
