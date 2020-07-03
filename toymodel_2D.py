b017dd4d8e14f7b90f04d218380a6f4b58885e20
davidwilson-85 <dws1985@hotmail.com>
2020-07-04 01:37:36 +0200

    -

--- toymodel_2D.py
+++ toymodel_2D.py
@@ -16,22 +16,25 @@ cuc_template = 'templates/...'
 # Parameters
 auxin_range = (0, 9)
 auxin_diffusionFactor = 0.1		# Relative amount of molecules that cross between two adjacent cells per cycle
-auxin_synthesis = 1     		# Absolute amount of molecules synthesized per cycle
+auxin_synthesis = 0.1     		# Absolute amount of molecules synthesized per cycle
+auxin_destruction = 0.1     	# Absolute amount of molecules destroyed per cycle
 auxin_lossRate = 0.75
 
 pin1_range = (0, 9)
 pin1_UTGresponsiveness = 0.01	# Relative amount of molecules that can change cell face per cycle
+pin1_transp = 0.1				# = Nbr auxin molecules transported / ( PIN1 molecule * cycle )
 
 cuc_range = (0, 1)
 
-nbr_iterations = 500
+nbr_iterations = 10
 
 
 # === LOAD DATA
 
 auxin = np.loadtxt(auxin_template, delimiter=',', unpack=False)
-matrix_shape = auxin.shape
-pin1 = np.loadtxt(pin1_template, delimiter=',', unpack=False).reshape((4,12,12)) # Format is [z,y,x]
+auxin_matrix_shape = auxin.shape
+pin1 = np.loadtxt(pin1_template, delimiter=',', unpack=False).reshape((4,11,11)) # Format is [z,y,x]
+pin1_matrix_shape = pin1.shape
 
 # LUTs
 lut_auxin = np.loadtxt('luts/lut_red.csv', delimiter=',', unpack=True, dtype=('int'), skiprows=1)
@@ -116,20 +119,30 @@ for iteration in range(nbr_iterations):
 	#create_heatmap(data=data)
 
 	# Plot data in cell tilling
-	create_cell_plot(matrix_shape)
+	create_cell_plot(auxin_matrix_shape)
+
 	
-	# Create absolute diffusion efflux values for each cell
-	diffusionVectors = np.zeros(matrix_shape, dtype=(float,1))	
-	for i in range(matrix_shape[0]):
-		for j in range(matrix_shape[1]):
+	# AUXIN DIFUSSION
+	#
+	# [auxin](i,j) = [auxin](i,j) - out_diff + in_diff_T + in_diff_R + in_diff_B + in_diff_L
+	#
+	# Rate of diffusion from cell i to j: dD(i->j)/dt = [auxin(i)] * k
+	#
+	# k = diffusion factor constant
+	#
+
+	# Create absolute efflux diffusion values for each cell
+	diffusionVectors = np.zeros(auxin_matrix_shape, dtype=(float,1))	
+	for i in range(auxin_matrix_shape[0]):
+		for j in range(auxin_matrix_shape[1]):
 			#print auxin[i,j]
 			diffusionVectors[i,j] = auxin[i,j] * auxin_diffusionFactor
 			
 	#print diffusionVectors
 	
-	# Calculate updated auxin concentration values
-	for i in range(matrix_shape[0]):
-		for j in range(matrix_shape[1]):
+	# Apply diffusion to auxin concentration values
+	for i in range(auxin_matrix_shape[0]):
+		for j in range(auxin_matrix_shape[1]):
 
 			# Count how many neighbours the cell has, to calculate the amount of efflux diffusion
 			nbr_cell_neighbours = 0
@@ -168,55 +181,119 @@ for iteration in range(nbr_iterations):
 			auxin[i,j] = auxin[i,j] - ( diffusionVectors[i,j] * nbr_cell_neighbours ) + diffusionFromLeft + diffusionFromRight + diffusionFromTop + diffusionFromBottom
 
 
+	# Apply PIN1 transport to auxin concentration values
+	#
+	# PIN1_Tr = Nbr auxin molecules / ( PIN1 molecule * cycle )
+	#
+	#
+	#
+
+	# Create absolute efflux transport values for each cell
+	transpVectors = np.zeros(pin1_matrix_shape, dtype=(float,1)) # 3D array = (cell_face, column, row)
+	
+	for y in range(pin1_matrix_shape[1]):
+		for x in range(pin1_matrix_shape[2]):
+			#print auxin[i,j]
+
+			total_molecules = auxin[y,x]
+			total_pin1 = pin1[0,y,x] + pin1[1,y,x] + pin1[2,y,x] + pin1[3,y,x]
+			transported_molecules = total_pin1 * pin1_transp
+
+			ratio = total_molecules / transported_molecules
+			if ratio > 1: ratio = 1
+
+
+			transpVectors[0,j,i] = auxin[y,x] * auxin_diffusionFactor
+	
+	for y in range(pin1_matrix_shape[0]):
+		for x in range(pin1_matrix_shape[1]):
+
+			# Top
+			transpFromTop = 0
+
+
+			# Update the auxin concentration in each cell
+			#auxin[i,j] = auxin[i,j] - ( transpVectors[i,j] * nbr_cell_neighbours ) + transpFromLeft + transpFromRight + transpFromTop + transpFromBottom
+
+	
+
+
+
 	# Calculate updated PIN1 allocation
-	for i in range(matrix_shape[0]):
-		for j in range(matrix_shape[1]):
+	for y in range(auxin_matrix_shape[0]):
+		for x in range(auxin_matrix_shape[1]):
 
 			# Left
-			if i-1 >= 0:
-				utg_auxinRatioL = math.sqrt(auxin[i-1,j] / auxin[i,j])
+			if x-1 >= 0:
+				utg_auxinRatioL = math.sqrt(auxin[y,x-1] / auxin[y,x])
 			else:
 				utg_auxinRatioL = 1.0
 			
 			# Right
 			try:
-				utg_auxinRatioR = math.sqrt(auxin[i+1,j] / auxin[i,j])
+				utg_auxinRatioR = math.sqrt(auxin[y,x+1] / auxin[y,x])
 			except IndexError:
 				utg_auxinRatioR = 1.0
 			
 			# Top	
-			if j-1 >= 0:
-				utg_auxinRatioT = math.sqrt(auxin[i,j-1] / auxin[i,j])
+			if y-1 >= 0:
+				utg_auxinRatioT = math.sqrt(auxin[y-1,x] / auxin[y,x])
 			else:
 				utg_auxinRatioT = 1.0
 			
 			# Bottom
 			try:
-				utg_auxinRatioB = math.sqrt(auxin[i,j+1] / auxin[i,j])
+				utg_auxinRatioB = math.sqrt(auxin[y+1,x] / auxin[y,x])
 			except IndexError:
 				utg_auxinRatioB = 1.0
 
-			# Calculate updated PIN1 concentration values per cell face
-			total_pin1 = pin1[0,i,j] + pin1[1,i,j] + pin1[2,i,j] + pin1[3,i,j]
+			# Calculate updated normalized PIN1 concentration values per cell face
+			total_pin1 = pin1[0,y,x] + pin1[1,y,x] + pin1[2,y,x] + pin1[3,y,x]
 
-			updated_pin1_T = pin1[0,j,i] * utg_auxinRatioT
-			updated_pin1_R = pin1[1,j,i] * utg_auxinRatioR
-			updated_pin1_B = pin1[2,j,i] * utg_auxinRatioB
-			updated_pin1_L = pin1[3,j,i] * utg_auxinRatioL
+			updated_pin1_T = pin1[0,y,x] * utg_auxinRatioT
+			updated_pin1_R = pin1[1,y,x] * utg_auxinRatioR
+			updated_pin1_B = pin1[2,y,x] * utg_auxinRatioB
+			updated_pin1_L = pin1[3,y,x] * utg_auxinRatioL
 
 			total_pin1_updated = updated_pin1_T + updated_pin1_R + updated_pin1_B + updated_pin1_L
 
 			normalization_ratio = total_pin1 / total_pin1_updated
 
-			pin1[0,j,i] = updated_pin1_T * normalization_ratio
-			pin1[1,j,i] = updated_pin1_R * normalization_ratio
-			pin1[2,j,i] = updated_pin1_B * normalization_ratio
-			pin1[3,j,i] = updated_pin1_L * normalization_ratio
+			# Normalize (force the sum of PIN1 in all faces in the cell to remain unchanged)
+			pin1[0,y,x] = updated_pin1_T * normalization_ratio
+			pin1[1,y,x] = updated_pin1_R * normalization_ratio
+			pin1[2,y,x] = updated_pin1_B * normalization_ratio
+			pin1[3,y,x] = updated_pin1_L * normalization_ratio
+
+			#pin1[0,0,5] = 9
+
+			#print pin1[0,y,x], pin1[1,y,x], pin1[2,y,x], pin1[3,y,x]
+
 
 
 	
 	auxin[3,3] = auxin[3,3] + auxin_synthesis
-	auxin[9,11] = auxin[9,11] + auxin_synthesis
+	auxin[10,5] = 0.5
+	#auxin[9,11] = auxin[9,11] + auxin_synthesis
 
+
+
+#===========================================
+# TO DO:
+# -Implement transport of auxin by PIN and see if instabilities in auxin distribution trigger patterning
+# -Create quick way of swutching ON/OFF features like UTG, WTF, CUC2
+# -Add auxin effect on PIN1 expression
+# 
+# 
+# matrix_shape[0] -> number of rows = y
+# matrix_shape[1] -> number of columns = x
+#
+# auxin[6,11] = 5 refers to 7th row, 12th column
+# pin1[0,0,5] = 5 refers to top wall of cell in 1st row, 6th column
+# 
+# 
+# 
+# 
+# 
 
 
