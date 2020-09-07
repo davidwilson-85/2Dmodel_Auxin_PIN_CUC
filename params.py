dd1329c9824d68a346a58fe314771add35261c4a
davidwilson-85 <dws1985@hotmail.com>
2020-09-07 23:09:18 +0200

    -

---
+++ params.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+
+# General
+nbr_iterations = 100
+img_dest_folder = 'images/test'
+cell_plot_frequency = 10
+
+# Switches
+PIN1_UTG = True
+PIN1_WTF = 'linear' # linear, cuadratic...
+AUX_LAX_transport = False
+CUC = False
+
+# Auxin
+auxin_range = (0, 9)        # This is only to map variable values to heatmap values
+k_auxin_diffusion = 0.3     # Relative amount of molecules that cross between two adjacent cells per cycle
+auxin_synthesis = 0.1       # Absolute amount of molecules synthesized per cycle
+auxin_destruction = 0.1     # Absolute amount of molecules destroyed per cycle
+auxin_noise_factor = 0
+
+# PIN1
+pin1_range = (0, 19)
+k_pin1_UTGresponsiveness = 0.01     # Relative amount of molecules that can change cell face per cycle
+k_pin1_transp = 0                   # = Nbr auxin molecules transported / ( PIN1 molecule * cycle ); used values=0.01
+
+# CUC
+cuc_range = (0, 9)                  # This is only to map variable values to heatmap values
+auxin_on_cuc = 0
+cuc_on_pin1Pol = 0
+
+
--- toymodel_2D.py
+++ toymodel_2D.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-#import tilings
 import os, shutil, random
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image, ImageDraw, ImageFont
+
+import params
 
 import func_graph
 import func_auxin
@@ -12,7 +13,13 @@ import func_cuc
 import func_pin
 
 
-# === SET PARAMETERS
+# Local synthesis or degradation (absolute or relative)
+	# Here define a list of elements, each specifying the cell coordinates, synth/degr, abs/rel, cycles, etc
+
+	# Example: marginSynthesis = new Local(type='synth', mode='abs', coords=(2,4,0), cycles=(0-4)) 
+
+
+# === LOAD TEMPLATE DATA
 
 # Templates
 auxin_template = 'templates/auxin_template'
@@ -20,53 +27,12 @@ pin1_template = 'templates/pin1_template'
 cuc_template = 'templates/cuc_template'
 middle_domain_template = 'templates/middle_domain_template'
 
-# Switches
-PIN1_UTG = True
-PIN1_WTF = 'linear' # linear, cuadratic...
-AUX_LAX_transport = False
-CUC = False
-
-# Parameters
-auxin_range = (0, 9)            # This is only to map variable values to heatmap values
-k_auxin_diffusion = 0.3			# Relative amount of molecules that cross between two adjacent cells per cycle
-auxin_synthesis = 0.1     		# Absolute amount of molecules synthesized per cycle
-auxin_destruction = 0.1     	# Absolute amount of molecules destroyed per cycle
-auxin_noise_factor = 0
-
-pin1_range = (0, 19)
-k_pin1_UTGresponsiveness = 0.01	# Relative amount of molecules that can change cell face per cycle
-###################################
-#
-# Add pin1_UTGresponsiveness to function!!!!!!!!!!!!!!! It is not used at all at the moment!!!!!!!!!!!!!!!!
-#
-###################################
-k_pin1_transp = 0					# = Nbr auxin molecules transported / ( PIN1 molecule * cycle ); used values=0.01
-
-cuc_range = (0, 9)				# This is only to map variable values to heatmap values
-auxin_on_cuc = 0
-cuc_on_pin1Pol = 0
-
-nbr_iterations = 1000
-img_dest_folder = 'images/test'
-cell_plot_frequency = 10
-
-# Local synthesis or degradation (absolute or relative)
-	# Here define a list of elements, each specifying the cell coordinates, synth/degr, abs/rel, cycles, etc
-
-	# Example: marginSynthesis = new Local(type='synth', mode='abs', coords=(2,4,0), cycles=(0-4)) 
-
-
-# === LOAD DATA
-
 auxin = np.loadtxt(auxin_template, delimiter=',', unpack=False)
 auxin_matrix_shape = auxin.shape
 tissue_rows, tissue_columns = auxin.shape[0], auxin.shape[1]
-
 pin1 = np.loadtxt(pin1_template, delimiter=',', unpack=False).reshape((4,tissue_rows,tissue_columns)) # Format is [z,y,x]
 pin1_matrix_shape = pin1.shape
-
 cuc = np.loadtxt(cuc_template, delimiter=',', unpack=False)
-
 middle_domain = np.loadtxt(middle_domain_template, delimiter=',', unpack=False)
 
 array_auxin_fluxes = np.zeros(shape=(10,tissue_rows,tissue_columns)) # Z: T_out, T_in, R_out, R_in...
@@ -86,7 +52,7 @@ print 'rows: ', tissue_rows
 '''
 # Tests
 
-for iteration in range(nbr_iterations):
+for iteration in range(params.nbr_iterations):
 
 	func_cuc.cuc_expression(middle_domain, auxin, cuc)
 	
@@ -102,7 +68,7 @@ quit()
 
 # Perform simulation cycles
 
-for iteration in range(nbr_iterations):
+for iteration in range(params.nbr_iterations):
 
 	print iteration
 
@@ -115,23 +81,37 @@ for iteration in range(nbr_iterations):
 	
 	# Cleanup destination folder (remove and create)
 	if iteration == 0:
-		shutil.rmtree(img_dest_folder) 
-		os.mkdir(img_dest_folder)
+		shutil.rmtree(params.img_dest_folder) 
+		os.mkdir(params.img_dest_folder)
 	
 	# Draw cell plot 
-	if iteration % cell_plot_frequency == 0:
-		func_graph.create_cell_plot(auxin_matrix_shape, auxin, auxin_range, lut_auxin, pin1, pin1_range, lut_pin1, cuc, cuc_range, lut_cuc, iteration, array_auxin_fluxes, img_dest_folder)
+	if iteration % params.cell_plot_frequency == 0:
+		func_graph.create_cell_plot(
+			auxin_matrix_shape,
+			auxin,
+			params.auxin_range,
+			lut_auxin,
+			pin1,
+			params.pin1_range,
+			lut_pin1,
+			cuc,
+			params.cuc_range,
+			lut_cuc,
+			iteration,
+			array_auxin_fluxes,
+			params.img_dest_folder
+		)
 	
 	#*************************************************************************************
 	
 	# Apply noise to auxin
 	
-	if auxin_noise_factor > 0:
+	if params.auxin_noise_factor > 0:
 	
 		for y in range(tissue_rows):
 			for x in range(tissue_columns):
 			
-				auxin[x,y] = auxin[x,y] + random.uniform(-auxin_noise_factor, auxin_noise_factor)
+				auxin[x,y] = auxin[x,y] + random.uniform(-params.auxin_noise_factor, params.auxin_noise_factor)
 			
 				if auxin[x,y] < 0:
 					auxin[x,y] = float(0.0000001)
@@ -152,9 +132,9 @@ for iteration in range(nbr_iterations):
 
 	# AUXIN DIFFUSION
 
-	if k_auxin_diffusion > 0:
+	if params.k_auxin_diffusion > 0:
 
-		func_auxin.auxin_diffusion(k_auxin_diffusion, auxin_matrix_shape, tissue_columns, tissue_rows, auxin, array_auxin_fluxes, iteration)
+		func_auxin.auxin_diffusion(params.k_auxin_diffusion, auxin_matrix_shape, tissue_columns, tissue_rows, auxin, array_auxin_fluxes, iteration)
 
 	#*************************************************************************************
 
@@ -169,17 +149,17 @@ for iteration in range(nbr_iterations):
 	
 	# PIN1 UTG
 
-	if k_pin1_UTGresponsiveness > 0:
+	if params.k_pin1_UTGresponsiveness > 0:
 
-		func_pin.auxin_on_pin_polarity(auxin, pin1, k_pin1_UTGresponsiveness, tissue_rows, tissue_columns)
+		func_pin.auxin_on_pin_polarity(auxin, pin1, params.k_pin1_UTGresponsiveness, tissue_rows, tissue_columns)
 
 	#*************************************************************************************
 
 	# PIN1-MEDIATED AUXIN EFFLUX
 
-	if k_pin1_transp > 0:
+	if params.k_pin1_transp > 0:
 
-		func_auxin.pin_on_auxin(auxin, pin1, k_pin1_transp, tissue_rows, tissue_columns, pin1_matrix_shape)
+		func_auxin.pin_on_auxin(auxin, pin1, params.k_pin1_transp, tissue_rows, tissue_columns, pin1_matrix_shape)
 	
 	
 
@@ -205,4 +185,7 @@ for iteration in range(nbr_iterations):
 # Set diffusion to 0 at faces that are outer boundaries
 # 
 # Check inner working of func_pin.auxin_on_pin_polarity()
+# 
+# Add pin1_UTGresponsiveness to function!!!!!!!!!!!!!!! It is not used at all at the moment!!!!!!!!!!!!!!!!
+# 
 
